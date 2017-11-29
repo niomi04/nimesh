b3d73d732476a882f1ae9ec3c3a85f51190fde3e
guptnime <guptnime@EABWVJPKJQ.asiapacific.hpqcorp.net>
2017-11-29 18:02:52 +0530

    First Commit

---
+++ FirstPROJECT/src/PACK1/test1.java
@@ -0,0 +1,16 @@
+package PACK1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		
+		
+		String str = "  hiii dear";
+		System.out.println(str);
+		
+		System.out.println(str.trim());
+	}
+
+}
